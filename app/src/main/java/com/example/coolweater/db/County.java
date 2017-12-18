f294bd2b60349670d39509fbb2ac34b9f5728e4c
wrh <wrh@qq.com>
2017-12-18 20:06:01 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweater/db/County.java
@@ -0,0 +1,41 @@
+package com.example.coolweater.db;
+
+public class County {
+    private int id;
+    private String countyName;
+    private String weatheiId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatheiId() {
+        return weatheiId;
+    }
+
+    public void setWeatheiId(String weatheiId) {
+        this.weatheiId = weatheiId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+}
